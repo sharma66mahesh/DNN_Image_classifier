cdd5933327f751d215276261c82ada3cf4f98d64
sharma66mahesh <gauravpudel1990@gmail.com>
2024-03-19 11:00:22 +0545

    Add forward propagation

--- model/LLayerModel.py
+++ model/LLayerModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import Tuple, List
+from utils.dnn_utils import *
 
 
 class LLayerModel:
@@ -7,7 +8,7 @@ class LLayerModel:
     Train, test and predict using L-layer Deep Neural Network
     """
 
-    def __init__(self, classes: List[str, str], num_iterations: int = 2000,
+    def __init__(self, classes: List[str], num_iterations: int = 2000,
                  learning_rate: float = 0.0075, print_cost: bool = True):
         """Initialize L-layer DNN with the provided layer_dims, classes, num_iterations, learning_rate and print_cost
 
@@ -22,7 +23,7 @@ class LLayerModel:
         self.learning_rate = learning_rate
         self.print_cost = print_cost
         self.costs = []
-        
+
         self.parameters = {}
 
     def fit(self, layer_dims: Tuple[int, ...], train_set_x: np.ndarray, train_set_y: np.ndarray,
@@ -44,10 +45,15 @@ class LLayerModel:
         self.layer_dims = layer_dims
         self.L = len(layer_dims) - 1
         self.m = train_set_x.shape[1]  # no. of training data
-        
+
+        # ensure that the model has all the data required
+        if not self.is_initialized():
+            raise Exception(
+                "Model might be missing train/test datasets or layer_dims")
+
         # initialize params
         self.parameters = self.initialize_parameters(layer_dims)
-        
+
         for i in range(self.num_iterations):
             # forward propagation
             pass
@@ -62,33 +68,9 @@ class LLayerModel:
         """Check if the model is initialized with all the necessary hyperparameters and datasets
 
         Returns:
-            bool: Bool status on the initialization status
+            bool: Bool value of the initialization status
         """
-        if not (self.train_set_x and self.train_set_y and self.test_set_x and self.test_set_y and self.layer_dims):
+        if not (len(self.train_set_x) > 0 and len(self.train_set_y) > 0 and len(self.test_set_x) > 0
+                and len(self.test_set_y) > 0 and len(self.layer_dims) > 0):
             return False
         return True
-
-    def initialize_parameters(layer_dims: Tuple[int, ...]):
-        """
-        Arguments:
-        layer_dims -- python array (list) containing the dimensions of each layer in our network. Eg: [n_x, 4,3,2,1], where 1st layer has 4 neurons, 2nd layer has 3 neurons, and so on...
-
-        Returns:
-        parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
-                        Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
-                        bl -- bias vector of shape (layer_dims[l], 1)
-        """
-        np.random.seed(1)
-        parameters = {}
-        L = len(layer_dims)
-
-        for l in range(1, L):
-            parameters['W' + str(l)] = np.random.randn(layer_dims[l],
-                                                       layer_dims[l-1]) / np.sqrt(layer_dims[l-1])
-            parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
-
-            assert (parameters['W' + str(l)].shape ==
-                    (layer_dims[l], layer_dims[l-1]))
-            assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
-
-        return parameters
--- utils/dnn_utils.py
+++ utils/dnn_utils.py
@@ -1,5 +1,165 @@
 import h5py
 import numpy as np
+from typing import Tuple, Dict
+
+def sigmoid(Z):
+    """
+    Implements the sigmoid activation in numpy
+
+    Arguments:
+    Z -- numpy array of any shape
+
+    Returns:
+    A -- output of sigmoid(z), same shape as Z
+    cache -- returns Z as well, useful during backpropagation
+    """
+    A = 1 / (1 + np.exp(-Z))
+    cache = Z
+
+    return A, cache
+
+
+def relu(Z):
+    """
+    Implement the RELU function.
+
+    Arguments:
+    Z -- Output of the linear layer, of any shape
+
+    Returns:
+    A -- Post-activation parameter, of the same shape as Z
+    cache -- Z; stored for computing the backward pass efficiently
+    """
+    A = np.maximum(0, Z)
+
+    assert (A.shape == Z.shape)
+
+    cache = Z
+    return A, cache
+
+
+def relu_backward(dA, cache):
+    """
+    Implement the backward propagation for a single RELU unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    Z = cache
+    dZ = np.array(dA, copy=True)  # just converting dZ to a correct object
+
+    # =============NOTE::================
+    # g'(z) = 0 if z <=0, else g'(z) = 1 ===> for ReLu
+    # dz = da * g'(z)
+
+    # when z <=0, dz = 0
+    dZ[Z <= 0] = 0
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
+
+
+def sigmoid_backward(dA, cache):
+    """
+    Implement the backward propagation for a single SIGMOID unit.
+
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    Z = cache
+
+    s = 1 / (1 + np.exp(Z))
+    # dz = da * g'(z), where g'(z) = g(z) * (1 - g(z)) ===> for sigmoid
+    dZ = dA * s * (1 - s + 10** -8)
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
+
+
+def initialize_parameters(self, layer_dims: Tuple[int, ...]) -> Dict:
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network. Eg: [n_x, 4,3,2,1], where 1st layer has 4 neurons, 2nd layer has 3 neurons, and so on...
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    bl -- bias vector of shape (layer_dims[l], 1)
+    """
+    np.random.seed(1)
+    parameters = {}
+    L = len(layer_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l],
+                                                   layer_dims[l-1]) / np.sqrt(layer_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+        assert (parameters['W' + str(l)].shape ==
+                (layer_dims[l], layer_dims[l-1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+# Layer XYZ to Layer XYZ + 1
+def linear_forward(A_prev, W, b):
+    """
+    Implement the linear part of a layer's forward propagation.
+
+    Arguments:
+    A -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+
+    Returns:
+    Z -- the input of the activation function, also called pre-activation parameter 
+    cache -- a python dictionary containing "A", "W" and "b" ; stored for computing the backward pass efficiently
+    """
+    Z = W.dot(A_prev) + b
+    assert (Z.shape == (W.shape[0], A_prev.shape[1]))
+
+    cache = (A_prev, W, b)
+    return Z, cache
+
+# Layer XYZ to Layer XYZ + 1
+def linear_activation_forward(A_prev, W, b, activation):
+    """
+    Implement the forward propagation for the LINEAR->ACTIVATION layer
+
+    Arguments:
+    A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    A -- the output of the activation function, also called the post-activation value 
+    cache -- a python dictionary containing "linear_cache" and "activation_cache";
+             stored for computing the backward pass efficiently
+    """
+    Z, linear_cache = linear_forward(A_prev, W, b)
+
+    if activation == "sigmoid":
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        A, activation_cache = relu(Z)
+
+    assert (Z.shape == (W.shape[0], A_prev.shape[1]))
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+
+    cache = (linear_cache, activation_cache)
+    return A, cache
+
 
 
 def load_dataset(train_dataset_path: str, test_dataset_path: str):
