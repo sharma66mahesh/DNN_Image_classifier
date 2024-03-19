6321c96a7eed8f368c0190ca7bd4fb702235db2d
sharma66mahesh <gauravpudel1990@gmail.com>
2024-03-19 11:01:13 +0545

    Add utils for cost computation

--- utils/dnn_utils.py
+++ utils/dnn_utils.py
@@ -160,6 +160,26 @@ def linear_activation_forward(A_prev, W, b, activation):
     cache = (linear_cache, activation_cache)
     return A, cache
 
+def compute_cost(AL, Y):
+    """
+    Implement the cross-entropy cost function
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (1, number of examples)
+    Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
+
+    Returns:
+    cost -- cross-entropy cost
+    """
+    m = Y.shape[1]
+    
+    cost = -1 / m * (np.dot(Y, np.log(AL).T) + np.dot((1 -Y), np.log(1-AL).T))
+    
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    return cost
+
 
 
 def load_dataset(train_dataset_path: str, test_dataset_path: str):
